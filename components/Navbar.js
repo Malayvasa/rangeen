dec72489c2f9a8c53419ef0587c9656577c7053a
malayvasa <malayvasa@gmail.com>
2023-04-22 18:45:37 +0530

    v1.5 New Navbar Animation

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,104 +12,120 @@ export default function NavBar() {
   const [currentPage, setCurrentPage] = useState('/');
   const router = useRouter();
 
+  const pages = [
+    {
+      name: 'Randomizer',
+      url: '/randomizer',
+      color: '#00ff00',
+    },
+    {
+      name: 'ColorGPT',
+      url: '/colorgpt',
+      color: '#ff0000',
+    },
+    {
+      name: 'Album Art',
+      url: '/album_art',
+      color: '#0000ff',
+    },
+  ];
+
   useEffect(() => {
     setCurrentPage(router.pathname);
   }, [router.pathname]);
 
   return (
     <div className="fixed z-30 top-0 w-screen flex h-max px-4 pt-10">
-      <div className="w-full md:w-3/4 items-center font-semibold justify-between rounded-full shadow-lg mx-auto top-0 h-12 px-4 pr-2 md:pr-4  bg-white flex z-20">
+      <div className="w-full md:w-3/5 items-center font-semibold justify-between rounded-full shadow-lg mx-auto top-0 h-12 pl-4 pr-2 md:pr-0  bg-white flex z-20">
         <div className="font-bold text-lg h-full">
           <Link href={'/'}>
             <HeaderLogo />
           </Link>
         </div>
-        <div className=" hidden md:flex h-full justify-center items-center text-neutral-600">
-          <div className="h-full">
-            {' '}
-            <Link
-              //if url is home page add a bottom border
-              className={`tracking-tight flex items-center px-2 hover:bg-gray-950/5 h-full`}
-              href="/randomizer"
-            >
-              Randomizer
-            </Link>
-            <div
-              //make bg blue if url is home page and transparent if not
 
-              className={`w-full h-[4px]  -mt-[2px] ${currentPage === '/randomizer'
-                ? 'bg-gray-500/20'
-                : 'bg-transparent'
-                }`}
-            ></div>
-          </div>
-
-          <div className="h-full">
-            <Link
-              className={`tracking-tight flex items-center px-2 hover:bg-gray-950/5 h-full `}
-              href="/colorgpt"
-            >
-              ColorGPT
-            </Link>
-            <div
-              //make bg blue if url is home page and transparent if not
-
-              className={`w-full h-[4px]  -mt-[2px] ${currentPage === '/colorgpt'
-                ? 'bg-gray-500/20'
-                : 'bg-transparent'
-                }`}
-            ></div>
-          </div>
-          <div className="h-full">
-            <Link
-              className={`tracking-tight flex items-center px-2 hover:bg-gray-950/5 h-full `}
-              href="/album_art"
-            >
-              Album Art
-            </Link>
-            <div
-              //make bg blue if url is home page and transparent if not
-
-              className={`w-full h-[4px]  -mt-[2px] ${currentPage === '/album_art'
-                ? 'bg-gray-500/20'
-                : 'bg-transparent'
-                }`}
-            ></div>
-          </div>
-        </div>
-
-        {session ? (
-          <div className="flex h-full gap-4">
-            <div className="h-full hidden md:block">
-              <Link
-                className={`tracking-tight flex items-center px-2 hover:bg-gray-950/5 h-full`}
-                href="/library"
-              >
-                Library
-              </Link>
+        <div className="flex h-full py-[4px] ">
+          {pages.map((page) => (
+            <div className="flex h-full gap-4">
+              <div className="h-full hidden md:block">
+                <Link
+                  className={`mx-1 group  relative flex items-center px-2 h-full`}
+                  href={page.url}
+                >
+                  {currentPage === page.url && (
+                    <motion.div
+                      layoutId="pill"
+                      className="absolute z-[6] cursor-none  inset-0 border-[2px] border-blue-500/10 bg-blue-400/30"
+                      style={{
+                        borderRadius: '9999px',
+                      }}
+                      transition={{
+                        duration: 0.5,
+                        type: 'spring',
+                      }}
+                    ></motion.div>
+                  )}
+                  <span
+                    className={`flex items-center bg-blend-exclusion text-black px-2 h-full z-10 transition-colors
+                    ${currentPage === page.url ? 'text-blue-500' : 'text-black'}
+                    `}
+                  >
+                    {page.name}
+                  </span>
+                </Link>
+              </div>
+            </div>
+          ))}
+          {session ? (
+            <div className="flex h-full gap-4">
+              <div className="h-full hidden md:block">
+                <Link
+                  className={`mx-1 group text-neutral-800 relative flex items-center px-2 h-full`}
+                  href="/library"
+                >
+                  {currentPage === '/library' && (
+                    <motion.div
+                      layoutId="pill"
+                      className="absolute z-[6] cursor-none  inset-0 border-[2px] border-blue-500/10 bg-blue-400/30"
+                      style={{
+                        borderRadius: '9999px',
+                      }}
+                      transition={{
+                        duration: 0.3,
+                        type: 'spring',
+                      }}
+                    ></motion.div>
+                  )}
+                  <span
+                    //if active page, make text white
+                    className={`flex items-center bg-blend-exclusion text-black px-2 h-full z-10 transition-colors
+                    ${
+                      currentPage === '/library'
+                        ? 'text-blue-500'
+                        : 'text-black'
+                    }
+                    `}
+                  >
+                    Library
+                  </span>
+                </Link>
+              </div>
               <div
-                //make bg blue if url is home page and transparent if not
-
-                className={`h-[4px] mx-auto -mt-[2px] ${currentPage === '/library'
-                  ? 'bg-gray-500/20'
-                  : 'bg-transparent'
-                  }`}
-              ></div>
+                className={`tracking-tight flex items-center md:hidden h-full`}
+              >
+                <MobileNav />
+              </div>
             </div>
-            <div className={`tracking-tight flex items-center md:hidden h-full`}>
-              <MobileNav />
+          ) : (
+            <div>
+              <Link
+                className="px-6 text-sm py-[11px] rounded-full bg-black/70 text-white tracking-tight"
+                href="/login"
+              >
+                Log In
+              </Link>
             </div>
-          </div>
-        ) : (
-          <div>
-            <Link
-              className="px-6 text-sm py-[11px] rounded-full bg-black/70 text-white tracking-tight"
-              href="/login"
-            >
-              Log In
-            </Link>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
